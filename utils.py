5665031ecd303e4c4754be822403448ef8188e46
uwagaDUDE <puvir00@gmail.com>
2023-03-14 16:46:40 +0300

    Реализован "удобный" просмотр вакансий

--- utils.py
+++ utils.py
@@ -1,5 +1,6 @@
 import requests
 import classes as cl
+
 def api_loader():
     """
     Загружаем api_key с сайта, чтобы он не светился в коде
@@ -13,3 +14,21 @@ def api_loader():
         api_key = None
         raise cl.AllErrors().ApiKeyError(api_key)
 
+def check_str(hh_attribute):
+    """
+    Проверка на то, является ли атрибут числом
+    :param hh_attribute: атрибут отвечающий за количество вакансий/страниц
+    :return: атрибут
+    """
+    if hh_attribute.isnumeric():
+        return hh_attribute
+    else:
+        raise cl.AllErrors().WrongInput()
+
+def check_max_len(hh_pages, hh_vacs):
+    if int(hh_vacs) > 20:
+        raise cl.AllErrors().WrongMaxValue()
+    elif int(hh_pages)*int(hh_vacs) > 2000:
+        raise cl.AllErrors().WrongMaxVacancies()
+    else:
+        pass
